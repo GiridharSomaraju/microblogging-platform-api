c52c4d1c873f2d9e7ed8f92e51fa0d66ce5b89b3
GiridharSomaraju <girisomaraju82@gmail.com>
2026-07-14 11:37:58 +0530

    feat: add global error handling middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,16 @@
+const errorHandler = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+
+    err.status = err.status || "error";
+
+    const message = 
+        err.message = 500 ? "Something went wrong. Please try again later" : err.message
+
+    res.status(err.statusCode).json({
+        success: false,
+        status: err.status,
+        message,
+    });
+};
+
+module.exports = errorHandler;
